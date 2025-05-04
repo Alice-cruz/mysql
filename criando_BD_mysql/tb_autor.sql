cea41635eaf17ec10e9381c63ec362acfa992818
Alice Cruz <65675761+Alice-cruz@users.noreply.github.com>
2025-05-03 21:40:33 -0300

    Update tb_autor.sql

--- criando_BD_mysql/tb_autor.sql
+++ criando_BD_mysql/tb_autor.sql
@@ -4,3 +4,5 @@ CREATE TABLE `autor` (
   `sobrenome_autor` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`idautor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--- criamos a tabela usando esses passos
